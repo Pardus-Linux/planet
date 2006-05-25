75f8ef1a8b1eaeac8bb3235c62a03f52c0bf704c
Gürer Özen <gurer@pardus.org.tr>
2006-05-25 09:00:28 +0000

    çılgın çocuk iso

--- planet.py
+++ planet.py
@@ -148,7 +148,7 @@ if __name__ == "__main__":
     # Sort the channels list by name
     channel_list = channels.values()
     locale.setlocale(locale.LC_ALL,"tr_TR.UTF-8")
-    channel_list.sort(key=lambda x: locale.strxfrm(x.name))
+    channel_list.sort(key=lambda x: locale.strxfrm(x["name"]))
     locale.setlocale(locale.LC_ALL,"C")    
 
 
